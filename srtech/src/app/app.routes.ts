9156e8fb80c288a47d41de46d959b75443fe5bc3
Sai-Rajana <Sai.rajana3@gmail.com>
2024-05-09 12:52:36 +0530

    init prod branch

--- srtech/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-
-export const routes: Routes = [
-    {path:'layout',loadChildren:()=> import('./layout/layout.module').then(m=> m.LayoutModule)},
-    {path:'',loadChildren:()=> import('./layout/layout.module').then(m=> m.LayoutModule)}
-];
